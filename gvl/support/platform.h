706f995e015ab6a25706575fe4e881545d266ab2
pelya <x.pelya.x@gmail.com>
2017-10-28 04:02:58 +0300

    Android modifications

--- gvl/support/platform.h
+++ gvl/support/platform.h
@@ -62,13 +62,15 @@
 #  define GVL_X86_64 1
 # elif defined(__i386__) || defined(_M_IX86) || defined(i386) || defined(i486) || defined(intel) || defined(x86) || defined(i86pc)
 #  define GVL_X86 1
+# elif defined(__ANDROID__)
+#  define GVL_LINUX 1
 # else
 #  error "Unknown architecture, please add it"
 # endif
 #endif
 
 #if !GVL_LITTLE_ENDIAN && !GVL_BIG_ENDIAN
-# if GVL_X86 || GVL_X86_64
+# if GVL_X86 || GVL_X86_64 || defined(__ANDROID__)
 #  define GVL_LITTLE_ENDIAN 1
 # else
 #  define GVL_BIG_ENDIAN 1
